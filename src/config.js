7b1000d13f152996f198e75155783ae6833fbf8e
Aditya Pratama Badra <orochimadit@gmail.com>
2022-01-25 14:28:28 +0700

    config

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-    api_host : 'http://localhost:3000',
-    site_title: 'Santri Penerima Amanah',
-    owner: 'IT Support Munzalan',
-    contact: 'adityapratamabadra@gmail.com'
+    api_host : process.env.REACT_APP_API_HOST,
+    site_title: process.env.REACT_APP_SITE_TITLE,
+    owner: process.env.REACT_APP_OWNER,
+    contact: process.env.REACT_APP_CONTACT
 }
 
 export {config};
